06ce96cb1c1c11d60bf64555e872fb097411a1a2
Roman Feldbauer <roman.feldbauer@ofai.at>
2016-07-29 15:33:43 +0200

    fixed multiclass handling and quadruples with equal distance

--- goodman_kruskal.m
+++ goodman_kruskal.m
@@ -3,7 +3,9 @@ function di = goodman_kruskal(D, classes)
 %
 % This file is part of the HUB TOOLBOX available at
 % http://ofai.at/research/impml/projects/hubology.html
+% https://github.com/OFAI/hub-toolbox-matlab/
 % (c) 2013, Dominik Schnitzer <dominik.schnitzer@ofai.at>
+% (c) 2016, Roman Feldbauer <roman.feldbauer@ofai.at>
 %
 % The Goodman-Kruskal index is a
 % clustering quality measure that relates the number of concordant ($Q_c$) and
@@ -30,6 +32,10 @@ function di = goodman_kruskal(D, classes)
     Qc = 0;
     Qd = 0;
     cls = unique(classes);
+    n = length(classes);
+    
+    % D_kl pairs in different classes
+    D_other = D(triu(repmat(classes', n, 1) ~= repmat(classes, 1, n)))';
     for c = 1:length(cls)
         
         sel = classes == cls(c);
@@ -37,28 +43,39 @@ function di = goodman_kruskal(D, classes)
             
             selD = false(size(D));
             selD(sel, sel) = true;
+            % D_ij pairs within same class
             D_self = D(triu(selD, 1))';
         else
             % skip if there is only one item per class
             continue;
         end
-        D_other = D(sel, ~sel);
-        D_other = D_other(:)';
+        % D_kl pairs in different classes (D_other) are computed once for all c
         D_full = [D_self D_other];
         
         self_size = length(D_self);
         other_size = length(D_other);
         
-        % Randomize, in case there are equal values
-        % (especially relevant for SNN rescaling)
-        rp = randperm(size(D_full,2));
-        d2 = D_full(rp);
-        [~, d2idx] = sort(d2);
-        full_idx = rp(d2idx);        
-        % OLD code, non-randomized
-        %[tmp, full_idx] = sort(D_full);
+        [~, full_idx] = sort(D_full);
         
-        
+        % Calc number of quadruples with equal distance
+        n_equidistant = 0;
+        sdf = sort(D_full);
+        equi_mask = boolean(zeros(size(sdf)));
+        % Positions with repeated values
+        equi_mask(2:end) = sdf(2:end) == sdf(end:-1:2);
+        equi_dist = sdf(equi_mask);
+        % How often does each value occur in self/other:
+        uniq_equi_dist = unique(equi_dist);
+        for i = 1:length(uniq_equi_dist)
+            dist = uniq_equi_dist(i);
+            equi_arg = find(D_full == dist);
+            self_equi = sum(equi_arg < self_size);
+            other_equi = length(equi_arg) - self_equi;
+            % Number of dc that are actually equal
+            n_equidistant = n_equidistant + self_equi * other_equi;
+        end
+
+        % Calc number of concordant quadruples
         cc = 0;
         ccsize = other_size;
         for i = 1:length(full_idx)
@@ -69,12 +86,14 @@ function di = goodman_kruskal(D, classes)
             end
         end
         
-        dc = self_size*other_size - cc;
+        % Calc number of discordant quadruples
+        dc = self_size*other_size - cc - n_equidistant;
         
         Qc = Qc + cc;
         Qd = Qd + dc;
     end
     
+    % Calc Goodman-Kruskal's gamma
     if (Qc+Qd == 0)
         di = 0;
     else
